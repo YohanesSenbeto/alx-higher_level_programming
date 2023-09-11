fa306992a5d17c8ca4e4132db8dd20c854494ff3
YohanesSenbeto <jonicasenbeto@gmail.com>
2023-09-11 12:01:23 -0700

    python-inheritance pro

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Defines a class-checking function."""
+
+
+def is_same_class(obj, a_class):
+    """check if an object is exact an instance of a give class.
+    Args:
+    obj (any): The object to check.
+    a_class (type): The class to match the type of object to.
+    Returns:
+      If obj is exact an instance of a_class - True.
+      otherwise - False.
+    """
+    if type(obj) == a_class:
+        return True
+    return False
+
