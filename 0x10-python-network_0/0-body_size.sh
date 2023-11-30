4d961370d8425ef70c7d8bda736234cb1f2c75d5
YohanesSenbeto <jonicasenbeto@gmail.com>
2023-11-30 09:58:11 -0800

    task 0 sent

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Usage message if no argument provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Get the size of the response body using curl and display in bytes
-curl -s "$1" | wc -c
+# takes URL, sends a request to that URL, displays a size of body of the response
+curl -sI GET "$1" | grep -i "Content-Length" | cut -d " " -f2
