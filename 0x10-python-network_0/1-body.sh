31f29aa6650021e6b9098dd95a893e1ae2f8d558
YohanesSenbeto <jonicasenbeto@gmail.com>
2023-11-30 10:29:52 -0800

    second file 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-# Check if a URL is provided as an argument
-if [ $# -eq 0 ]; then
-  echo "Please provide a URL as an argument."
-  exit 1
-fi
-
-# Store a URL from a command-line argument
-url="$1"
-
-# Send a GET request and display a response body
-curl -sX GET "$url"
+# Takes a URL, sends a GET request to the URL, and displays the body of the response.
+curl -sX GET "$1" -L 200
