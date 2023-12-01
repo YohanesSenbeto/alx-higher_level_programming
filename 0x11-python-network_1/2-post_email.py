824b2996e302d7e2adc19feb07b260f336e6400b
YohanesSenbeto <jonicasenbeto@gmail.com>
2023-12-01 12:00:18 -0800

    updated one

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
 
 """
-script sends a POST request to a given URL with an email as a parameter & displays the body of the response.
+This script sends a POST request to a given URL with an email as a parameter and displays the body of the response.
 """
 
+from urllib import request, parse
+import sys
 
 if __name__ == "__main__":
-    from urllib.request import urlopen,Request
-    from urllib.parse import urlencode
-    from sys import argv
+    # Checking if the correct number of arguments are passed
+    if len(sys.argv) != 3:
+        print("Usage: ./script_name.py <URL> <email>")
+        sys.exit(1)
+    
+    # Extracting URL and email from command line arguments
+    url = sys.argv[1]
+    email = sys.argv[2]
     
     # Encoding the email parameter
-    value = parse.urlencode({'email': email})
-    request = Request(
-            argv[1], urlencode(value).encode("ascii"))
+    data = parse.urlencode({'email': email})
+    data = data.encode('utf-8')  # Encoding the data to bytes
     
     # Sending a POST request to the URL with the email as a parameter using urllib and a with statement
-    with urlopen(request) as response:
+    with request.urlopen(url, data) as response:
         # Reading and decoding the body of the response
-        body = response.headers.get('x-Request-id')
-        print(response.read().decode('utf-8'))
+        body = response.read().decode('utf-8')
+        print(body)
