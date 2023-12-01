d899a4dadd1fc65051767acfd9257566f08fb019
YohanesSenbeto <jonicasenbeto@gmail.com>
2023-12-01 11:57:07 -0800

    Update two task

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
 
 """
-This script sends that request to a URL and displays the value of the X-Request-Id variable found in the response header.
+script sends that request to URL & displays the value of X-Request-Id variable found in response header.
 """
 
-from urllib import request
-import sys
 
 if __name__ == "__main__":
-    # Checking if a URL is provided as an argument
-    if len(sys.argv) < 2:
-        print("Usage: ./script_name.py <URL>")
-        sys.exit(1)
-    
+    import urllib.request
+    import sys
+      
     url = sys.argv[1]
     
     # Sending a request to the URL using urllib and a with statement
-    with request.urlopen(url) as response:
+    with urllib.request.urlopen(url) as response:
         # Retrieving the value of the X-Request-Id variable from the response header
         x_request_id = response.getheader('X-Request-Id')
         
-        if x_request_id:
-            print(x_request_id)
-        else:
-            print("X-Request-Id header not found in the response.")
+        print(x_request_id)
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
 
 """
-This script sends a POST request to a given URL with an email as a parameter and displays the body of the response.
+script sends a POST request to a given URL with an email as a parameter & displays the body of the response.
 """
 
-from urllib import request, parse
-import sys
 
 if __name__ == "__main__":
-    # Checking if the correct number of arguments are passed
-    if len(sys.argv) != 3:
-        print("Usage: ./script_name.py <URL> <email>")
-        sys.exit(1)
-    
-    # Extracting URL and email from command line arguments
-    url = sys.argv[1]
-    email = sys.argv[2]
+    from urllib.request import urlopen,Request
+    from urllib.parse import urlencode
+    from sys import argv
     
     # Encoding the email parameter
-    data = parse.urlencode({'email': email})
-    data = data.encode('utf-8')  # Encoding the data to bytes
+    value = parse.urlencode({'email': email})
+    request = Request(
+            argv[1], urlencode(value).encode("ascii"))
     
     # Sending a POST request to the URL with the email as a parameter using urllib and a with statement
-    with request.urlopen(url, data) as response:
+    with urlopen(request) as response:
         # Reading and decoding the body of the response
-        body = response.read().decode('utf-8')
-        print(body)
+        body = response.headers.get('x-Request-id')
+        print(response.read().decode('utf-8'))
