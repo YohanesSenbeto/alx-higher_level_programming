07efac9e395c173f7f5b0cc4ab0227c2899ef141
YohanesSenbeto <jonicasenbeto@gmail.com>
2024-01-10 21:42:48 +0000

    updated last

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,22 @@
+const $ = window.$;
+
+$(document).ready(function() {
+  $('#btn_translate').click(function() {
+    translateHello();
+  });
+
+  $('#language_code').keypress(function(event) {
+    if (event.which === 13) {
+      translateHello();
+    }
+  });
+
+  function translateHello() {
+    const languageCode = $('#language_code').val();
+    const apiUrl = `https://www.fourtonfish.com/hellosalut/hello/${languageCode}`;
+
+    $.get(apiUrl, function(data) {
+      $('#hello').text(data.hello);
+    });
+  }
+});
