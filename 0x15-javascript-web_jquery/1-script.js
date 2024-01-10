0b1acf3f5a7a3312d49860e9ceb0fcd214c5b094
YohanesSenbeto <jonicasenbeto@gmail.com>
2024-01-10 21:25:44 +0000

    updated

--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -1,12 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('header').style.color = '#FF0000';
-    document.querySelector('DIV#red_header').addEventListener('click', function () {
-      document.querySelector('header').style.color = '#FF0000';
-    });
-    document.querySelector('DIV#green_header').addEventListener('click', function () {
-      document.querySelector('header').style.color = '#00FF00';
-    });
-    document.querySelector('DIV#blue_header').addEventListener('click', function () {
-      document.querySelector('header').style.color = '#0000FF';
-    });
-  });
+$(document).ready(function() {
+  $('header').css('color', '#FF0000');
+});
